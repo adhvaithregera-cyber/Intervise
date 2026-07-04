a2cd95fd21b83f6e712259055230ed122bef2537
adhvaithregera-cyber <adhvaithregera@gmail.com>
2026-07-04 12:55:30 +0530

    fix: use Heart icon temporarily (lucide-react issue with Instagram icon)

--- components/ui/minimal-footer.tsx
+++ components/ui/minimal-footer.tsx
@@ -1,6 +1,6 @@
 import {
     Lightbulb,
-    Instagram,
+    Heart,
 } from 'lucide-react';
 
 export function MinimalFooter() {
@@ -54,7 +54,7 @@ export function MinimalFooter() {
 
     const socialLinks = [
         {
-            icon: <Instagram className="size-4" />,
+            icon: <Heart className="size-4" />,
             link: 'https://www.instagram.com/intervisehq/',
             label: 'Instagram',
         },
